bdbcd1b269f9e7482b0b7f9fc98a0e4cd4540a18
anugrahdh <71118889+anugrahdh@users.noreply.github.com>
2020-09-19 22:19:39 +0700

    Pertemuan Ke-2

---
+++ Assets/Latihan3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan3 : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int c = Jumlah(60, 40);
+        Debug.Log("Hasil Jumlah a dan b adalah " + c);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// Merupakan fungsi penjumlahan dari 2 input angka
+    /// </summary>
+    /// <param name="a">masukkan angka interger</param>
+    /// <param name="b">masukkan angka interger</param>
+    /// <returns></returns>
+    int Jumlah(int a, int b)
+    {
+        return a + b;
+    }
+}
