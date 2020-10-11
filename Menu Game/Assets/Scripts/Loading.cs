c895a318615bb02069e6596e5e0dd3f81c14183c
anugrahdh <71118889+anugrahdh@users.noreply.github.com>
2020-10-12 03:15:04 +0700

    Menu Game

---
+++ Menu Game/Assets/Scripts/Loading.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Loading : MonoBehaviour
+{
+    //waktu delay untuk load scene
+    [SerializeField] private float delayLoading = 3f;
+
+    [SerializeField] private string namaScene;
+
+    private float timeElapsed;
+
+    //  nama scene untuk pindah scene yang diinputkan pada Inspector
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > delayLoading)
+        {
+            SceneManager.LoadScene(namaScene);
+        }
+    }
+
+
+
+}
---
+++ Menu Game/Assets/Scripts/Scene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    // pindah scene ke menu
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    // pindah scene ke loading
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+    // keluar dari game
+    public void KeluarScene()
+    {
+        Application.Quit();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
