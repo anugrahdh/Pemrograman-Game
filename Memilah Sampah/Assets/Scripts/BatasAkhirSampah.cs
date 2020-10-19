821460cd255fe8a34f93b52a35dff0827d95bdf0
anugrahdh <71118889+anugrahdh@users.noreply.github.com>
2020-10-19 20:52:40 +0700

    Memilah Sampah

---
+++ Memilah Sampah/Assets/Scripts/BatasAkhirSampah.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BatasAkhirSampah : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D Collision)
+    {
+        Destroy(Collision.gameObject);
+        SceneManager.LoadScene("GameOver"); // Baris ini akan digunakkan pada submodel GameOver
+    }
+}
